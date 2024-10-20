861a470579007c2187fc9b4d788be70d35fe3aa1
OttoAllright <Ottocrkavre@gmail.com>
2024-10-20 19:11:49 -0400

    right

---
+++ server.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import fs from 'fs';
+import path from 'path';
+import multer from 'multer';
+import { compressAudio } from './audioCompressor.js';
+const upload = multer({ dest: './public/uploads/'});
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.post('/api', upload.single('audio'), (req,res)=>{
+    console.log('audio arrived', req.file)
+    try{
+            const inputFilePath = req.file.path;
+            const outputFilePath = path.join('./public/uploads', `compressed_${req.file.filename}.mp3`);
+
+            compressAudio(inputFilePath, outputFilePath)
+            .then(compressedFilePath => {
+                res.json({ "downloadUrl": `uploads/compressed_${req.file.filename}.mp3` });
+            
+            
+                     // Esperar 15 segundos antes de eliminar los archivos
+                     setTimeout(() => {
+                        // Eliminar el archivo de entrada
+                        fs.unlink(inputFilePath, (err) => {
+                            if (err) {
+                                console.error("Error deleting input file:", err);
+                            } else {
+                                console.log("Input file deleted successfully.");
+                            }
+                        });
+        
+                        // Eliminar el archivo de salida
+                        fs.unlink(outputFilePath, (err) => {
+                            if (err) {
+                                console.error("Error deleting output file:", err);
+                            } else {
+                                console.log("Output file deleted successfully.");
+                            }
+                        });
+                    }, 15000); // 15 segundos
+                })
+                .catch(err => {
+                    console.error("Error compressing audio:", err);
+                    res.status(500).send("Error processing file.");
+                });
+    } catch(error){
+            res.status(500).send('Error processing');
+        
+    }
+
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
