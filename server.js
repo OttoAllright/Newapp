da4291c1a6ded55912c82fb2fd4bf48dd5ae4f64
OttoAllright <Ottocrkavre@gmail.com>
2024-10-21 14:18:29 -0400

    Update server.js

--- server.js
+++ server.js
@@ -15,55 +15,51 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/api', (req, res) => {
-    res.send('API está funcionando');
-
-
-    app.post('/api', upload.single('audio'), (req,res)=>{
-        console.log('audio arrived', req.file)
-        try{
-                const inputFilePath = req.file.path;
-                const outputFilePath = path.join('public/uploads', `compressed_${req.file.filename}.mp3`);
-    
-                compressAudio(inputFilePath, outputFilePath)
-                .then(compressedFilePath => {
-                    res.json({ "downloadUrl": `uploads/compressed_${req.file.filename}.mp3` });
-                
-                
-                         // Esperar 15 segundos antes de eliminar los archivos
-                         setTimeout(() => {
-                            // Eliminar el archivo de entrada
-                            fs.unlink(inputFilePath, (err) => {
-                                if (err) {
-                                    console.error("Error deleting input file:", err);
-                                } else {
-                                    console.log("Input file deleted successfully.");
-                                }
-                            });
-            
-                            // Eliminar el archivo de salida
-                            fs.unlink(outputFilePath, (err) => {
-                                if (err) {
-                                    console.error("Error deleting output file:", err);
-                                } else {
-                                    console.log("Output file deleted successfully.");
-                                }
-                            });
-                        }, 15000); // 15 segundos
-                    })
-                    .catch(err => {
-                        console.error("Error compressing audio:", err);
-                        res.status(500).send("Error processing file.");
-                    });
-        } catch(error){
-                res.status(500).send('Error processing');
-            
-        }
-    
-    })
-    
+    res.send('API está funcionando');    
 });
 
+app.post('/api', upload.single('audio'), (req,res)=>{
+    console.log('audio arrived', req.file)
+    try{
+            const inputFilePath = req.file.path;
+            const outputFilePath = path.join('public/uploads', `compressed_${req.file.filename}.mp3`);
 
+            compressAudio(inputFilePath, outputFilePath)
+            .then(compressedFilePath => {
+                res.json({ "downloadUrl": `uploads/compressed_${req.file.filename}.mp3` });
+            
+            
+                     // Esperar 15 segundos antes de eliminar los archivos
+                     setTimeout(() => {
+                        // Eliminar el archivo de entrada
+                        fs.unlink(inputFilePath, (err) => {
+                            if (err) {
+                                console.error("Error deleting input file:", err);
+                            } else {
+                                console.log("Input file deleted successfully.");
+                            }
+                        });
+        
+                        // Eliminar el archivo de salida
+                        fs.unlink(outputFilePath, (err) => {
+                            if (err) {
+                                console.error("Error deleting output file:", err);
+                            } else {
+                                console.log("Output file deleted successfully.");
+                            }
+                        });
+                    }, 15000); // 15 segundos
+                })
+                .catch(err => {
+                    console.error("Error compressing audio:", err);
+                    res.status(500).send("Error processing file.");
+                });
+    } catch(error){
+            res.status(500).send('Error processing');
+        
+    }
+
+})
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
